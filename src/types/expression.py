7e52ea54d8849ecf364c0efdcb27a9b524997d74
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-30 18:17:53 +0200

    Implement expression string representation

--- src/types/expression.py
+++ src/types/expression.py
@@ -1,10 +1,19 @@
-class Expression():
-    def __init__(self, _type: str, _value, _line, _column, _lineEnd, _columnEnd):
-        self.type = _type
-        self.value = _value
-        self.line = _line
-        self.column = _column
-        self.lineEnd = _lineEnd
-        self.columnEnd = _columnEnd
+from ..util import indent
 
-        
+
+class Expression():
+	def __init__(self, _type: str, _value, _line, _column, _lineEnd, _columnEnd):
+		self.type = _type
+		self.value = _value
+		self.line = _line
+		self.column = _column
+		self.lineEnd = _lineEnd
+		self.columnEnd = _columnEnd
+
+	def toString(self, indentation):
+		i = indent(indentation)
+		value = f"'{self.value}'" if isinstance(self.value, str) else self.value
+		return f"{i}{self.type}({value})"
+
+	def __str__(self):
+		return self.toString(0)
--- src/types/function.py
+++ src/types/function.py
@@ -1,10 +1,20 @@
+from ..util import indent
 from .expression import Expression
 
 
 class FunctionCallExpression(Expression):
-    def __init__(self, _name, _args, _line, _column, _lineEnd, _columnEnd):
-        super().__init__("FunctionCallExpression", None, _line, _column, _lineEnd, _columnEnd)
-        self.name = _name
-        self.args = _args
-        self.body = None
-        self.env = None
+	def __init__(self, _name: str, _args: list[Expression], _line: int, _column: int, _lineEnd: int, _columnEnd: int):
+		super().__init__("FunctionCallExpression", None, _line, _column, _lineEnd, _columnEnd)
+		self.name = _name
+		self.args = _args
+
+	def toString(self, indentation):
+		i = indent(indentation)
+		s = f"{i}{self.__class__.__name__}('{self.name}', \n"
+		for arg in self.args:
+			s += f"{arg.toString(indentation + 1)},\n"
+		s += i + ")"
+		return s
+
+	def __str__(self):
+		return self.toString(0)
