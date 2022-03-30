80f1ba51b4e57f22f51cdb3033bd5a20c674691c
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-30 17:57:18 +0200

    Format and add Token type

--- src/compiler.py
+++ src/compiler.py
@@ -1,6 +1,7 @@
 from os import write
 from termcolor import colored as c
 
+from .types.token import Token
 from .types.program import Program
 from .util import path_filename, path_normalize, path_validate, print_error
 from .lexer import tokenize
@@ -8,54 +9,56 @@ from .parser import parse
 from .checker import check
 from .generator import generate
 
-def printTokens(tokens: list):
-    print(c("=== Tokens ===", "cyan"))
-    for t in tokens:
-        print(t['type'].ljust(10), '|', t['value'])
+def printTokens(tokens: list[Token]):
+	print(c("=== Tokens ===", "cyan"))
+	for t in tokens:
+		print(t)
 
 def printProgram(program: Program):
-    print(c("=== Program ===", "cyan"))
-    print("Lines:", program.lines)
-    print("Imports:")
-    for i in program.imports:
-        print("    ", i)
-    if len(program.imports) == 0: print("  None")
-    print("AST:")
-    for b in program.body:
-        print("    ", b)
-    if len(program.body) == 0: print("  None")
+	print(c("=== Program ===", "cyan"))
+	print("Lines:", program.lines)
+	print("Imports:")
+	for i in program.imports:
+		print("    ", i)
+	if len(program.imports) == 0: print("  None")
+	print("AST:")
+	for b in program.body:
+		print("    ", b.__class__.__name__)
+	if len(program.body) == 0: print("  None")
 
 def printCommands(commands):
-    print(c("=== Commands ===", "cyan"))
-    print(commands)
+	print(c("=== Commands ===", "cyan"))
+	print(commands)
 
 def printTypeCheck():
-    print(c("=== Type Check ===", "cyan"))
-    print("Type checking successful!")
+	print(c("=== Type Check ===", "cyan"))
+	print("Type checking successful!")
 
 def compile(filepath: str, debug: bool = False):
-    filepath = path_normalize(filepath)
-    filename = path_filename(filepath)
-    path_validate(filepath)
-    print(c(f"Compiling ", "cyan") + c(f"'{filename}'", "yellow") + c(f"...", "cyan"))
-    errorType = "Unknown"
-    try:
-        errorType = "Syntax"
-        tokens = tokenize(filepath, debug)
-        if debug:
-            printTokens(tokens)
-        errorType = "Semantic"
-        program = parse(tokens, debug)
-        if debug:
-            printProgram(program)
-        errorType = "Type"
-        check(program, debug)
-        if debug:
-            printTypeCheck()
-        errorType = "Generate"
-        output = generate(program, debug)
-        if debug:
-            printCommands(output)
-        # TOTO: Output to file
-    except Exception as e:
-        print_error(errorType, filename, e)
+	filepath = path_normalize(filepath)
+	filename = path_filename(filepath)
+	path_validate(filepath)
+	print(c(f"Compiling ", "cyan") + c(f"'{filename}'", "yellow") + c(f"...", "cyan"))
+	errorType = "Unknown"
+	try:
+		errorType = "Syntax"
+		tokens = tokenize(filepath, debug)
+		if debug:
+			printTokens(tokens)
+		errorType = "Semantic"
+		program = parse(tokens, debug)
+		if debug:
+			printProgram(program)
+		errorType = "Type"
+		check(program, debug)
+		if debug:
+			printTypeCheck()
+		errorType = "Generate"
+		output = generate(program, debug)
+		if debug:
+			printCommands(output)
+		# TOTO: Output to file
+	except Exception as e:
+		print_error(errorType, filename, e, not debug)
+		if debug:
+			raise e
