2123a44f2baaca2aa5ef4c2c9b544b1c4a9b16a7
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-30 17:57:14 +0200

    Create token.py

---
+++ src/types/token.py
@@ -0,0 +1,17 @@
+class Token():
+	def __init__(self, _type: str, _value, _line: int, _column: int, _lineEnd: int, _columnEnd: int, _next: str):
+		self.type = _type
+		self.value = _value
+		self.line = _line
+		self.column = _column
+		self.lineEnd = _lineEnd
+		self.columnEnd = _columnEnd
+		self.next = _next
+
+	def __str__(self):
+		return self.type.ljust(10) + ' | ' + self.value
+
+class OperatorToken(Token):
+	def __init__(self, _subtype: str, _value, _line: int, _column: int, _lineEnd: int, _columnEnd: int):
+		super().__init__('Operator', _value, _line, _column, _lineEnd, _columnEnd, None)
+		self.subtype = _subtype
