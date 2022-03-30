e7570c35274a0948ca35724f20d564bc861ba727
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-30 14:43:36 +0200

    Add more examples

---
+++ examples/greet_nearest.c
@@ -0,0 +1,7 @@
+import player from minecraft
+
+say(
+    # Comments are supported
+    @r(type=minecraft:player, radius<=10),
+    "Hello {@s}!"
+)
--- examples/say_hello.c
+++ examples/say_hello.c
@@ -1,7 +1,1 @@
-import player from minecraft
-
-say(
-    # Comments are supported
-    r(type=minecraft:player, radius<=10),
-    "Hello {@s}!"
-)
+print("Hello ", @s, "!")
