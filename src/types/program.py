1d883822d827183b40582c3c6cabafd610111174
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-30 16:29:26 +0200

    Add types

--- src/types/program.py
+++ src/types/program.py
@@ -1,8 +1,9 @@
+from .statements import ImportStatement
 from .expression import Expression
 
 
 class Program(Expression):
-    def __init__(self, _imports: list, _body: list, _lines: int):
+    def __init__(self, _imports: list[ImportStatement], _body: list[Expression], _lines: int):
         self.type = "Program"
         self.imports = _imports
         self.body = _body
