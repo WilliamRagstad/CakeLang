b6449af36bf01b60090111912980d7799abf1946
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-31 15:26:18 +0200

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -189,6 +189,7 @@ def parse_import():
 			getToken()
 	getToken() # Consume the from keyword
 	module = expectNext("Identifier")
+	dprint(f"parse_import: {subjects} from '{module.value}'")
 	return ImportStatement(module, subjects, start.line, start.column, module.lineEnd, module.columnEnd)
 
 def parse(_tokens: list[Token], _filepath: str, _debug: bool = False) -> Program:
